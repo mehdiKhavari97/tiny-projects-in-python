f1c5267c4756c7c60f805492ad6f9e2a309ad045
Mehdi Khavari <me.khavari.97@gmail.com>
2021-06-30 23:09:53 +0430

    Add files via upload

---
+++ paint_effect.py
@@ -0,0 +1,11 @@
+import cv2
+
+img = cv2.imread('./pic.jpg')
+gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+inverted_gray_image = 255-gray_image
+blurred_img = cv2.GaussianBlur(inverted_gray_image, (21,21), 0)
+inverted_blurred_image = 255-blurred_img
+pincel_sketch_IMG = cv2.divide(gray_image, inverted_blurred_image, scale=256.0)
+
+cv2.imshow('Original Image', pincel_sketch_IMG)
+cv2.waitKey(0)
